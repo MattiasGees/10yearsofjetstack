b352ebd323a70269ccccca5d82788c70b946f2ad
Charlie Egan <charlieegan3@users.noreply.github.com>
2025-02-24 09:28:24 +0000

    Add some photos
    Signed-off-by: Charlie Egan <charlieegan3@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "storage-console";
+
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "86e1ad4ec007f4f0e9561886935fe9b278860de8";
+    };
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+      rev = "b1d9ab70662946ef0850d488da1c9019f3a9752a";
+    };
+    pre-commit-hooks = {
+      type = "github";
+      owner = "cachix";
+      repo = "git-hooks.nix";
+      rev = "c7012d0c18567c889b948781bc74a501e92275d1";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    let
+      utils = flake-utils;
+    in
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        checks = {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              dprint = {
+                enable = true;
+                name = "dprint check";
+                entry = "dprint check --allow-no-files";
+              };
+              nixfmt = {
+                enable = true;
+                name = "nixfmt check";
+                entry = "nixfmt -c ";
+                types = [ "nix" ];
+              };
+            };
+          };
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+
+            env = {
+              DOCKER_HOST = "unix:///Users/charlieegan3/.colima/default/docker.sock";
+              TESTCONTAINERS_RYUK_DISABLED = "true";
+            };
+
+            packages = with pkgs; [
+              python3
+
+              dprint
+              nixfmt-rfc-style
+            ];
+          };
+        };
+      }
+    );
+}
